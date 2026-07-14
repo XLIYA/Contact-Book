e7a1354d7d1ca4d19557e4366c3654a3b1348089
ILYA LEO <Leoag8434@gmail.com>
2026-07-14 20:09:31 +0330

    search and total count

---
+++ src/hooks/use-debounce.js
@@ -0,0 +1,17 @@
+// src/hooks/use-debounce.js
+
+import { useState, useEffect } from "react";
+
+export const useDebounce = (value, delay = 400) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
